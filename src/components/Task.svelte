21afd14da42ca73ac1137165817b03db4a257170
Tom Coleman <tom@thesnail.org>
2019-11-18 11:52:56 +1100

    Built out Task

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -9,7 +9,6 @@
       id: task.id,
     });
   }
-
   // event handler for Archive Task
   function ArchiveTask() {
     dispatch('onArchiveTask', {
@@ -26,8 +25,23 @@
     updated_at: new Date(2019, 0, 1, 9, 0),
   };
   //
+  // reactive declaration (computed prop in other frameworks)
+  $: isChecked = task.state === 'TASK_ARCHIVED';
 </script>
 
-<div class="list-item">
-  <input type="text" value={task.title} readonly />
+<div class={`list-item ${task.state}`}>
+  <label class="checkbox">
+    <input type="checkbox" checked={isChecked} disabled name="checked" />
+    <span class="checkbox-custom" on:click={ArchiveTask} />
+  </label>
+  <div class="title">
+    <input type="text" readonly value={task.title} placeholder="Input title" />
+  </div>
+  <div class="actions">
+    {#if task.state !== 'TASK_ARCHIVED'}
+      <a href="/" on:click|preventDefault={PinTask}>
+        <span class="icon-star" />
+      </a>
+    {/if}
+  </div>
 </div>
