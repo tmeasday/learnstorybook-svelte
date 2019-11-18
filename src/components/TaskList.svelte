6690d6606c266c24905bc25155fe1fc462bd5807
Tom Coleman <tom@thesnail.org>
2019-11-18 12:08:05 +1100

    Filled out TaskList

--- src/components/TaskList.svelte
+++ src/components/TaskList.svelte
@@ -1,21 +1,38 @@
 <script>
   import Task from './Task.svelte';
+  import LoadingRow from './LoadingRow.svelte';
   export let loading = false;
   export let tasks = [];
 
-  // reactive declarations (computed prop in other frameworks)
+  // reactive declaration (computed prop in other frameworks)
   $: noTasks = tasks.length === 0;
   $: emptyTasks = tasks.length === 0 && !loading;
+  $: tasksInOrder = [
+    ...tasks.filter(t => t.state === 'TASK_PINNED'),
+    ...tasks.filter(t => t.state !== 'TASK_PINNED'),
+  ];
 </script>
 
 {#if loading}
-  <div class="list-items">loading</div>
+  <div class="list-items">
+    <LoadingRow />
+    <LoadingRow />
+    <LoadingRow />
+    <LoadingRow />
+    <LoadingRow />
+  </div>
 {/if}
 
-{#if emptyTasks}
-  <div class="list-items">empty</div>
+{#if tasks.length === 0 && !loading}
+  <div class="list-items">
+    <div class="wrapper-message">
+      <span class="icon-check" />
+      <div class="title-message">You have no tasks</div>
+      <div class="subtitle-message">Sit back and relax</div>
+    </div>
+  </div>
 {/if}
 
-{#each tasks as task}
+{#each tasksInOrder as task}
   <Task {task} on:onPinTask on:onArchiveTask />
 {/each}
